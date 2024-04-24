1c5b82dbfba315c917112264dfad0e53d60992bd
KoZeuh <kozeuhdev@gmail.com>
2024-04-24 12:09:22 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/product/show/{product}', [App\Http\Controllers\ProductController::class, 'show']);
+    Route::get('/product/show/{product}', [ProductController::class, 'show']);
     Route::get('/admin', [AdminController::class, 'index']);
+
+    Route::get('/cart', [CartController::class, 'showCart']);
+    Route::post('/cart/addOrUpdate', [CartController::class, 'addToCart']);
+    Route::get('/cart/remove/{product}', [CartController::class, 'removeFromCart']);
+
+    Route::get('/checkout', [CartController::class, 'showCheckout']);
 });
 
 
